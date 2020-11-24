098bcdbdc6cc3e3f2b850b70bd6dc426c8aaadd7
BenStigsen <benjamin.stigsen@gmail.com>
2020-11-24 02:40:38 +0100

    Planet changes - Added name property to each planet - Added a dictionary to keep track of planet index

--- assets/js/objects.js
+++ assets/js/objects.js
@@ -58,84 +58,104 @@ function getTriangleArea(x1, y1, x2, y2, x3, y3)
 }
 
 sun = {
-  d: 1_392_700,
-  r: 696_350 / SCALE_DIVISOR, 
-  color: "#FFDD00"
+  d:      1_392_700,
+  r:      696_350, 
+  color:  "#FFDD00"
 }
 
 mercury = {
-  a: 57_900_000,
-  e: 0.205,
-  au: 0.387,
-  d: 4879,
-  color: "#333333"
+  a:      57_900_000,
+  e:      0.205,
+  au:     0.387,
+  d:      4879,
+  color:  "#333333",
+  name:   "Mercury"
 }
 
 venus = {
-  a: 108_200_000,
-  e: 0.007,
-  au: 0.723,
-  d: 12104,
-  color: "#FFDDDD"
+  a:      108_200_000,
+  e:      0.007,
+  au:     0.723,
+  d:      12104,
+  color:  "#FFDDDD",
+  name:   "Venus"
 }
 
 earth = {
-  a:  149_600_000,  // km
-  e:  0.0167,
-  au: 1,
-  d:  12_756,       // km
-  color: "#00FF00"
+  a:      149_600_000,  // km
+  e:      0.0167,
+  au:     1,
+  d:      12_756,       // km
+  color:  "#00FF00",
+  name:   "Earth"
 }
 
 mars = {
-  a:  227_920_000,
-  e:  0.094,
-  au: 1.52,
-  d:  6792,
-  color: "#FF7F00"
+  a:      227_920_000,
+  e:      0.094,
+  au:     1.52,
+  d:      6792,
+  color:  "#FF7F00",
+  name:   "Mars"
 }
 
 jupiter = {
-  a:  778_570_000,
-  e:  0.0489,
-  au: 5.20,
-  d:  142_984,
-  color: "#555555"
+  a:      778_570_000,
+  e:      0.0489,
+  au:     5.20,
+  d:      142_984,
+  color:  "#555555",
+  name:   "Jupiter"
 }
 
 saturn = {
-  a: 1_433_500_000,
-  e: 0.057,
-  au: 9.58,
-  d: 120_536,
-  color: "#FFFF00"
+  a:      1_433_500_000,
+  e:      0.057,
+  au:     9.58,
+  d:      120_536,
+  color:  "#FFFF00",
+  name:   "Saturn"
 }
 
 uranus = {
-  a: 2_872_500_000,
-  e: 0.046,
-  au: 19.20,
-  d: 51_118,
-  color: "#FFFF00"
+  a:      2_872_500_000,
+  e:      0.046,
+  au:     19.20,
+  d:      51_118,
+  color:  "#FFFF00",
+  name:   "Uranus"
 }
 
 neptune = {
-  a: 4_495_100_000,
-  e: 0.011,
-  au: 30.05,
-  d: 49528,
-  color: "#2288FF"
+  a:      4_495_100_000,
+  e:      0.011,
+  au:     30.05,
+  d:      49528,
+  color:  "#2288FF",
+  name:   "Neptune"
 }
 
 pluto = {
-  a: 5_906_400_000,
-  e: 0.244,
-  au: 39.48,
-  d: 2370,
-  color: "#2222FF"
+  a:      5_906_400_000,
+  e:      0.244,
+  au:     39.48,
+  d:      2370,
+  color:  "#2222FF",
+  name:   "Pluto"
 }
 
 
 // Array of planets for easy iteration
 // Earth is first to apply setup changes to it
-planets = [earth, mercury, venus, mars, jupiter, saturn, uranus, neptune, pluto];
+planets = [
+  mercury, venus, earth,
+  mars, jupiter, saturn, 
+  uranus, neptune, pluto
+];
+
+// Dictionary of reference to each planet index in array
+planetsRef = {}
+for (let i = 0; i < planets.length; i++)
+{
+  planetsRef[planets[i].name] = i;
+}
